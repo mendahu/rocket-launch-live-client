bee5936da672f16cbf64d1461cadfdfa0645bddc
Jake Robins <me@jakerobins.com>
2023-02-01 10:23:06 -0600

    add JSDoc documentation

--- src/Client.ts
+++ src/Client.ts
@@ -36,6 +36,18 @@ class RLLClient {
     }
   }
 
+  /**
+   * Used internally to make API query
+   *
+   * @private
+   *
+   * @template T
+   *
+   * @param {string} endpoint - API endpoint (ie. "/launches")
+   * @param {URLSearchParams} params - API Search Params
+   *
+   * @returns {Promise<T>}
+   */
   private query<T>(endpoint: string, params: URLSearchParams): Promise<T> {
     return fetcher<T>(
       this.apiKey,
@@ -45,81 +57,204 @@ class RLLClient {
     );
   }
 
+  /**
+   * Fetch launch companies
+   *
+   * @public
+   * @function
+   *
+   * @param {Object} [options] - Launch Company Search Options
+   * @param {number | string} options.id - Company id
+   * @param {number | string} options.page - Page number of results
+   * @param {number | string} options.name - Company name
+   * @param {ISO3166Alpha2.CountryCode} options.country_code - ISO 3166 Alpha 2 Country Code
+   * @param {boolean} options.inactive - Company inactive status
+   *
+   * @returns {Promise<RLLResponse<RLLEntity.Company[]>>} Array of companies wrapped in a standard RLL Response
+   *
+   * @example
+   *
+   * const response = client.companies({ country_code: "US" })
+   */
   public companies(
     options?: RLLQueryConfig.Companies
-  ): Promise<RLLResponse<RLLEntity.Company[] | RLLEntity.Company>> {
+  ): Promise<RLLResponse<RLLEntity.Company[]>> {
     return queryOptionsValidator(RLLEndPoint.COMPANIES, options).then(
       (params) =>
-        this.query<RLLResponse<RLLEntity.Company[] | RLLEntity.Company>>(
+        this.query<RLLResponse<RLLEntity.Company[]>>(
           RLLEndPoint.COMPANIES,
           params
         )
     );
   }
 
+  /**
+   * Fetch launches
+   *
+   * @public
+   * @function
+   *
+   * @param {Object} [options] - Launch Search Options
+   * @param {number | string} options.id - Launch id
+   * @param {number | string} options.page - Page number of results
+   * @param {string} options.cospar_id - Launch COSPAR ID (ie. 2022-123)
+   * @param {Date | string} options.before_date - Only return launches before this date
+   * @param {Date | string} options.after_date - Only return launches after this date
+   * @param {Date | string} options.modified_since - Only return launches with API changes after this date
+   * @param {number | string} options.location_id - Launches from this Location
+   * @param {number | string} options.pad_id - Launches from this Pad
+   * @param {number | string} options.provider_id - Launches from this Company
+   * @param {number | string} options.tag_id - Launches with this Tag
+   * @param {number | string} options.vehicle_id - Launches on this Vehicle
+   * @param {ISO3166Alpha2.StateCodeUS} options.state_abbr - ISO 3166 Alpha 2 US State Code
+   * @param {ISO3166Alpha2.CountryCode} options.country_code - ISO 3166 Alpha 2 Country Code
+   * @param {number | string} options.search - Launches matching this search string
+   * @param {number | string} options.slug - Launches matching this unique slug
+   *
+   * @returns {Promise<RLLResponse<RLLEntity.Launch[]>>} Array of companies wrapped in a standard RLL Response
+   *
+   * @example
+   *
+   * const response = client.launches({ after_date: new Date("2022-10-10") })
+   */
   public launches(
     options?: RLLQueryConfig.Launches
-  ): Promise<RLLResponse<RLLEntity.Launch[] | RLLEntity.Launch>> {
+  ): Promise<RLLResponse<RLLEntity.Launch[]>> {
     return queryOptionsValidator(RLLEndPoint.LAUNCHES, options).then((params) =>
-      this.query<RLLResponse<RLLEntity.Launch[] | RLLEntity.Launch>>(
-        RLLEndPoint.LAUNCHES,
-        params
-      )
+      this.query<RLLResponse<RLLEntity.Launch[]>>(RLLEndPoint.LAUNCHES, params)
     );
   }
 
+  /**
+   * Fetch launch locations
+   *
+   * @public
+   * @function
+   *
+   * @param {Object} [options] - Launch Location Search Options
+   * @param {number | string} options.id - Location id
+   * @param {number | string} options.page - Page number of results
+   * @param {number | string} options.name - Location name
+   * @param {ISO3166Alpha2.StateCodeUS} options.state_abbr - ISO 3166 Alpha 2 US State Code
+   * @param {ISO3166Alpha2.CountryCode} options.country_code - ISO 3166 Alpha 2 Country Code
+   *
+   * @returns {Promise<RLLResponse<RLLEntity.Location[]>>} Array of companies wrapped in a standard RLL Response
+   *
+   * @example
+   *
+   * const response = client.locations({ country_code: "US" })
+   */
   public locations(
     options?: RLLQueryConfig.Locations
-  ): Promise<RLLResponse<RLLEntity.Location[] | RLLEntity.Location>> {
+  ): Promise<RLLResponse<RLLEntity.Location[]>> {
     return queryOptionsValidator(RLLEndPoint.LOCATIONS, options).then(
       (params) =>
-        this.query<RLLResponse<RLLEntity.Location[] | RLLEntity.Location>>(
+        this.query<RLLResponse<RLLEntity.Location[]>>(
           RLLEndPoint.LOCATIONS,
           params
         )
     );
   }
+
+  /**
+   * Fetch launch Missions
+   *
+   * @public
+   * @function
+   *
+   * @param {Object} [options] - Launch Mission Search Options
+   * @param {number | string} options.id - Mission id
+   * @param {number | string} options.page - Page number of results
+   * @param {number | string} options.name - Mission name
+   *
+   * @returns {Promise<RLLResponse<RLLEntity.Mission[]>>} Array of companies wrapped in a standard RLL Response
+   *
+   * @example
+   *
+   * const response = client.missions({ name: "Mars 2020" })
+   */
   public missions(
     options?: RLLQueryConfig.Missions
-  ): Promise<RLLResponse<RLLEntity.Mission[] | RLLEntity.Mission>> {
+  ): Promise<RLLResponse<RLLEntity.Mission[]>> {
     return queryOptionsValidator(RLLEndPoint.MISSIONS, options).then((params) =>
-      this.query<RLLResponse<RLLEntity.Mission[] | RLLEntity.Mission>>(
-        RLLEndPoint.MISSIONS,
-        params
-      )
+      this.query<RLLResponse<RLLEntity.Mission[]>>(RLLEndPoint.MISSIONS, params)
     );
   }
 
+  /**
+   * Fetch launch pads
+   *
+   * @public
+   * @function
+   *
+   * @param {Object} [options] - Launch Pad Search Options
+   * @param {number | string} options.id - Pad id
+   * @param {number | string} options.page - Page number of results
+   * @param {number | string} options.name - Pad name
+   * @param {ISO3166Alpha2.StateCodeUS} options.state_abbr - ISO 3166 Alpha 2 US State Code
+   * @param {ISO3166Alpha2.CountryCode} options.country_code - ISO 3166 Alpha 2 Country Code
+   *
+   * @returns {Promise<RLLResponse<RLLEntity.Pad[]>>} Array of companies wrapped in a standard RLL Response
+   *
+   * @example
+   *
+   * const response = client.pads({ country_code: "US" })
+   */
   public pads(
     options?: RLLQueryConfig.Pads
-  ): Promise<RLLResponse<RLLEntity.Pad[] | RLLEntity.Pad>> {
+  ): Promise<RLLResponse<RLLEntity.Pad[]>> {
     return queryOptionsValidator(RLLEndPoint.PADS, options).then((params) =>
-      this.query<RLLResponse<RLLEntity.Pad[] | RLLEntity.Pad>>(
-        RLLEndPoint.PADS,
-        params
-      )
+      this.query<RLLResponse<RLLEntity.Pad[]>>(RLLEndPoint.PADS, params)
     );
   }
 
+  /**
+   * Fetch launch tags
+   *
+   * @public
+   * @function
+   *
+   * @param {Object} [options] - Launch Tag Search Options
+   * @param {number | string} options.id - Tag id
+   * @param {number | string} options.page - Page number of results
+   * @param {number | string} options.text - Tag text
+   *
+   * @returns {Promise<RLLResponse<RLLEntity.Tag[]>>} Array of companies wrapped in a standard RLL Response
+   *
+   * @example
+   *
+   * const response = client.tags({ text: "Crewed" })
+   */
   public tags(
     options?: RLLQueryConfig.Tags
-  ): Promise<RLLResponse<RLLEntity.Tag[] | RLLEntity.Tag>> {
+  ): Promise<RLLResponse<RLLEntity.Tag[]>> {
     return queryOptionsValidator(RLLEndPoint.TAGS, options).then((params) =>
-      this.query<RLLResponse<RLLEntity.Tag[] | RLLEntity.Tag>>(
-        RLLEndPoint.TAGS,
-        params
-      )
+      this.query<RLLResponse<RLLEntity.Tag[]>>(RLLEndPoint.TAGS, params)
     );
   }
 
+  /**
+   * Fetch launch Vehicles
+   *
+   * @public
+   * @function
+   *
+   * @param {Object} [options] - Launch Vehicles Search Options
+   * @param {number | string} options.id - Vehicle id
+   * @param {number | string} options.page - Page number of results
+   * @param {number | string} options.name - Vehicle name
+   *
+   * @returns {Promise<RLLResponse<RLLEntity.Vehicle[]>>} Array of companies wrapped in a standard RLL Response
+   *
+   * @example
+   *
+   * const response = client.vehicles({ name: "Falcon 9" })
+   */
   public vehicles(
     options?: RLLQueryConfig.Vehicles
-  ): Promise<RLLResponse<RLLEntity.Vehicle[] | RLLEntity.Vehicle>> {
+  ): Promise<RLLResponse<RLLEntity.Vehicle[]>> {
     return queryOptionsValidator(RLLEndPoint.VEHICLES, options).then((params) =>
-      this.query<RLLResponse<RLLEntity.Vehicle[] | RLLEntity.Vehicle>>(
-        RLLEndPoint.VEHICLES,
-        params
-      )
+      this.query<RLLResponse<RLLEntity.Vehicle[]>>(RLLEndPoint.VEHICLES, params)
     );
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import RLLClient from "./Client";
 import { RLLClientOptions } from "./types/application";
 
+/**
+ * Generate a RocketLaunch.Live client
+ *
+ * @public
+ * @function
+ *
+ * @param {string} apiKey - Your RocketLaunch.Live API Key
+ * @param {Object} [options] - Optional Client Configuration options
+ * @param {boolean} options.keyInQueryParams - Set to true to send your API Key via Query parameters instead of Authorization Header (not recommended)
+ *
+ * @returns {RLLCLient}
+ *
+ * @example
+ *
+ * const MY_KEY = process.env.ROCKETLAUNCH_LIVE_API_KEY
+ *
+ * const client = clientGenerator(MY_KEY, { keyInQueryParams: true })
+ */
 const clientGenerator = (
   apiKey: string,
   options?: RLLClientOptions
